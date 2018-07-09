ea809f80ae2c2afc6920427146eb93ada4684c57
taojianghang <taojianghang@crnet.com.hk>
2018-07-09 13:44:39 +0800

    增加网络相关的代码

--- src/main/net/net-sripts.py
+++ src/main/net/net-sripts.py
@@ -1,1 +1,16 @@
 # 练习Python访问互联网的API
+
+import requests
+import json
+
+base_url = "https://api.github.com"
+
+def get_url(url):
+
+    return base_url+url
+
+resp = requests.get(get_url("/users?page=0&per_page=1'"))
+
+result = resp.text
+
+print(resp.url , "返回结果为:" , json.dumps(result)) 
